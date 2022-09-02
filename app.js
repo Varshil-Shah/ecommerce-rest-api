8d27d0659f2238d32a5a6808fcf3a575cf564122
Varshil-Shah <varshilashah09@gmail.com>
2022-09-02 17:16:12 +0530

    Add middleware to handle unknown routes

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ const app = express();
 // logging every request on console in development mode
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
+app.use('*', (req, res, next) => {
+  // eslint-disable-next-line no-console
+  res.send({ message: `Cannot found ${req.originalUrl} on server.` });
+});
+
 module.exports = app;
