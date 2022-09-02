dcd18609d5debbbc2818a75f9dbf26de1322248d
Varshil-Shah <varshilashah09@gmail.com>
2022-09-02 17:43:44 +0530

    Connect mongo db and add dotenv

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 /* eslint-disable no-console */
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
 process.on('uncaughtException', (error) => {
   console.log('Uncaught Exception Occurred');
   console.log({ unhandledRejectionError: error });
   process.exit(1);
 });
+
+dotenv.config({
+  path: './config.env',
+});
+
+const MONGO_DATABASE_URL = process.env.MONGO_DATABASE_URL.replace(
+  'PASSWORD',
+  process.env.MONGO_DATABASE_PASSWORD
+).replace('USER', process.env.MONGO_DATABASE_USER);
+
+console.log({ MONGO_DATABASE_URL });
+
+mongoose
+  .connect(MONGO_DATABASE_URL)
+  .then(() => {
+    console.log('Database connected successfully!');
+  })
+  .catch((error) => {
+    console.error({ error });
+    console.log('Error while connecting database!');
+  });
 
 const app = require('./app');
 
