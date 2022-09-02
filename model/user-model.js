0544b5da08b6a61ef44dab7b1902180d0b63a3f9
Varshil-Shah <varshilashah09@gmail.com>
2022-09-02 23:17:32 +0530

    Design schema for user

---
+++ model/user-model.js
@@ -0,0 +1,101 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const UserSchema = mongoose.Schema({
+  location: {
+    type: {
+      type: String,
+      default: 'Point',
+      enum: ['Point'],
+    },
+    coordinates: [Number],
+    street: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide description.'],
+      minlength: [10, 'Minimum 10 characters required for street.'],
+      maxlength: [50, 'Maximum 50 characters allowed for street.'],
+    },
+    city: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide a valid city.'],
+      minlength: [3, 'Minimum 3 characters required for city.'],
+      maxlength: [30, 'Maximum 30 characters allowed for city.'],
+      set: (value) => String(value).toLowerCase(),
+    },
+    zipcode: {
+      type: Number,
+      required: false,
+      validate: {
+        validator: (value) => Number.isNaN(value),
+        message: 'Please enter a valid zipcode',
+      },
+    },
+    country: {
+      type: String,
+      required: [true, 'Please provide a valid country.'],
+      trim: true,
+      minlength: [5, 'Minimum 5 characters required for country.'],
+      maxlength: [30, 'Maximum 30 characters allowed for country.'],
+    },
+  },
+  name: {
+    firstName: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide a first name.'],
+      maxlength: [30, 'Maximum 30 characters allowed for first name.'],
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide a last name.'],
+      maxlength: [30, 'Maximum 30 characters allowed for last name.'],
+    },
+  },
+  username: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: [true, 'Please provide a username.'],
+    minlength: [8, 'Minimum 8 characters required for username.'],
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: [true, 'Please provide a email address.'],
+    validate: {
+      validator: (value) => !validator.isEmail(value),
+      message: 'Please enter a valid email address.',
+    },
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    required: [true, 'Please provide a valid role.'],
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: [true, 'Please provide a password.'],
+    validate: {
+      validator: (value) => !validator.isStrongPassword(value),
+      message:
+        'Password must be at least 8 characters long, 1 Uppercase, 1 Lowercase and 1 special character',
+    },
+  },
+  gender: {
+    type: String,
+    required: [true, 'Please provide a valid gender.'],
+    enum: {
+      values: ['male', 'female', 'other'],
+      message: 'Please provide a valid gender',
+    },
+  },
+});
+
+const UserModel = mongoose.model('Users', UserSchema);
+
+module.exports = UserModel;
