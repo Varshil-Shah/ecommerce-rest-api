9adb5c1901d89a384f0842c547b06161003ed7fa
Varshil-Shah <varshilashah09@gmail.com>
2022-09-07 09:31:16 +0530

    Implement a feature to getAllProdct with api-features

--- controller/product-controller.js
+++ controller/product-controller.js
@@ -1,10 +1,23 @@
 const Product = require('../model/product-model');
 const catchAsync = require('../utils/catch-async');
 const StatusCode = require('../utils/status-code');
+const ApiFeatures = require('../utils/api-features');
+
 // const AppError = require('../utils/app-error');
 
+const mapOfFilters = new Map([
+  ['productId', '_id'],
+  ['creatorId', 'creator._id'],
+  ['username', 'creator.username'],
+  ['email', 'creator.email'],
+  ['firstName', 'creator.name.firstName'],
+  ['lastName', 'creator.name.lastName'],
+  ['categoryId', '_id'],
+  ['categoryName', 'category.name'],
+]);
+
 exports.getAllProducts = catchAsync(async (req, res, next) => {
-  const products = await Product.find();
+  const products = await ApiFeatures(req.query, Product, mapOfFilters);
 
   res.status(StatusCode.OK).json({
     status: 'success',
